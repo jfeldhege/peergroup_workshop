1b5f928651d19657db2e44f3558ec99d52ff5ed6
jfeldhege <jfeldhege+git@gmail.com>
2026-05-12 20:46:23 +0200

    add images to slides

---
+++ examples/lintr_example.R
@@ -0,0 +1,13 @@
+# fmt: skip file
+
+average<-mean(feet/12 + inches,na.rm=T)
+sqrt(x ^ 2 + y ^ 2) # Long comment that goes over the set length at 80 characters
+
+base :: get  
+mtcars$mpg == NA
+
+a <- 1; b <- 1
+
+c('a', 'b')
+
+x = 5
